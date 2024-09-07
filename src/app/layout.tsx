77c7d27933545d3e64bc50f0e4b28c2e4edb5290
simon0820s <simon0820s@gmail.com>
2024-09-07 18:56:51 -0500

    feat(walletConnect): add button to connect wallet

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,17 +1,11 @@
-import type { Metadata } from "next";
-import localFont from "next/font/local";
 import "./globals.css";
+import type { Metadata } from "next";
+import { headers } from "next/headers";
 
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-});
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
-});
+import { cookieToInitialState } from "wagmi";
+
+import { config } from "@/config";
+import AppKitProvider from "@/context";
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -23,12 +17,11 @@ export default function RootLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const initialState = cookieToInitialState(config, headers().get("cookie"));
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
+      <body>
+        <AppKitProvider initialState={initialState}>{children}</AppKitProvider>
       </body>
     </html>
   );
--- src/app/page/Nav.tsx
+++ src/app/page/Nav.tsx
@@ -1,9 +1,10 @@
+import WalletConnectButton from "@/components/connectWalletButton";
 
 export default function Nav() {
-    return (
-      <div>
-        <h1>NAV PAGEEE</h1>
-      </div>
-    );
-  }
-  
+  return (
+    <div>
+      <h1>NAV PAGEEE</h1>
+      <WalletConnectButton />
+    </div>
+  );
+}
