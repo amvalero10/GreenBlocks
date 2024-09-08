d8380591259c532850fd67e5d859616dfa5044f0
simon0820s <simon0820s@gmail.com>
2024-09-08 08:22:30 -0500

    feat(itemDetail): add button to go back

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -1,67 +1,55 @@
-
 "use client";
 
-
 import { useState } from "react";
-import { useRouter } from 'next/navigation';  // Cambiado a 'next/navigation'
-import Box from '@mui/material/Box';
-import BottomNavigation from '@mui/material/BottomNavigation';
-import BottomNavigationAction from '@mui/material/BottomNavigationAction';
-import StorefrontIcon from '@mui/icons-material/Storefront';
-import HomeIcon from '@mui/icons-material/Home';
-
-
+import { useRouter } from "next/navigation"; // Cambiado a 'next/navigation'
+import Box from "@mui/material/Box";
+import BottomNavigation from "@mui/material/BottomNavigation";
+import BottomNavigationAction from "@mui/material/BottomNavigationAction";
+import StorefrontIcon from "@mui/icons-material/Storefront";
+import HomeIcon from "@mui/icons-material/Home";
 
 export default function Home() {
+  const [value, setValue] = useState(0);
+  const router = useRouter(); // Usa el router del paquete 'next/navigation'
 
+  const handleChange = (event: any, newValue: any) => {
+    setValue(newValue);
+    switch (newValue) {
+      case 0:
+        router.push("/home"); // Ruta para la página de inicio
+        break;
+      case 1:
+        router.push("/"); // Ruta para la página del mercado
+        break;
+      default:
+        break;
+    }
+  };
 
-    const [value, setValue] = useState(0);
-    const router = useRouter();  // Usa el router del paquete 'next/navigation'
-
-    const handleChange = (event: any, newValue:any) => {
-        setValue(newValue);
-        switch (newValue) {
-          case 0:
-            router.push('/home'); // Ruta para la página de inicio
-            break;
-          case 1:
-            router.push('/'); // Ruta para la página del mercado
-            break;
-          default:
-            break;
-        }
-      };
-
-
-
-    return (
-        <div>
-
-<Box      sx={{
-      width: 500,
-      margin: '0 auto', 
-      display: 'flex',
-      justifyContent: 'center', 
-    }}>
-        <BottomNavigation
-            value={value}
-            onChange={handleChange}
-        >
-            <BottomNavigationAction label="Home" icon={<HomeIcon sx={{ fontSize: 30 }}/>} />
-            <BottomNavigationAction label="Market" icon={<StorefrontIcon sx={{ fontSize: 30 }}/>} />
+  return (
+    <div>
+      <Box
+        sx={{
+          width: 500,
+          margin: "0 auto",
+          display: "flex",
+          justifyContent: "center",
+        }}
+      >
+        <BottomNavigation value={value} onChange={handleChange}>
+          <BottomNavigationAction
+            label="Home"
+            icon={<HomeIcon sx={{ fontSize: 30 }} />}
+          />
+          <BottomNavigationAction
+            label="Market"
+            icon={<StorefrontIcon sx={{ fontSize: 30 }} />}
+          />
         </BottomNavigation>
-        </Box>
+      </Box>
 
-
-
-
-            <h1>Home</h1>
-            <p>Este es el contenido de la página de inicio.</p>
-        </div>
-    );
-
-
-
-
-
+      <h1>Home</h1>
+      <p>Este es el contenido de la página de inicio.</p>
+    </div>
+  );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,28 +1,27 @@
-import Nav from "../components/Nav";
 import Navbar from "../components/navbar";
+import WalletConnectButton from "@/components/connectWalletButton";
 import Market from "../components/Market";
 
 export default function Home() {
   return (
     <div>
-
-      <div>
+      <div className="z-10 fixed top-4 right-4">
+        <WalletConnectButton />
+      </div>
+      
+      <div className="z-10">
         <Navbar />
       </div>
 
       <div>
-      <Market />
+        <Market />
       </div>
 
-
-      <Nav />
       <h1>GreenBlocks</h1>
       <p>
         GreenBlocks is a project that aims to help people to reduce their carbon
         footprint by providing them with a platform to buy and sell used items.
       </p>
-
-
     </div>
   );
 }
